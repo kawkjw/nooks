cd9ce33fd5d4e83aaf7c2a65c7415c209efcb235
Jinwoo Kawk <tkfkdnwjd@gmail.com>
2020-10-23 15:24:26 +0900

    useTabs

---
+++ useTabs/useTabs.js
@@ -0,0 +1,5 @@
+export const useTabs = (initialTab, allTabs) => {
+    if (!allTabs || !Array.isArray(allTabs)) return;
+    const [currentIndex, setCurrentIndex] = useState(initialTab);
+    return { currentItem: allTabs[currentIndex], changeItem: setCurrentIndex };
+};
